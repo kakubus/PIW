bfffafa08609ef89945e9d1bdc6be7cb5e2450bf
Kuba <kakubus@outlook.com>
2021-11-12 12:22:07 +0100

    Poprawki
    Troche nieskuteczne

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ import random
 import numpy as np
 import ctypes  # An included library with Python install.   
 
+counter_of_colors = 0
+list_of_colors = []
+
+def generateColor(rng, i):
+    global counter_of_colors
+    counter_of_colors+=1
+    return (np.random.choice(range(rng), size=i))
 
 
 
-image = cv2.imread('shapes.png', 0)
+
+image = cv2.imread('shapes2.png', 0)
 
 image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
 image_new = image
-list_of_colors = []
-list_of_colors.append((64,64,64)) #poczytac o listach, bo trzeba zrobic zliczanie kolorkow
+
+#list_of_colors.append((64,64,64)) #poczytac o listach, bo trzeba zrobic zliczanie kolorkow
+
 counter_without_problems = 0
-counter_of_colors = 1
+
 new_color = list(np.random.choice(range(256), size=3))
+list_of_colors.append(generateColor(256,3))
 
 rows = image.shape[0]
 cols = image.shape[1]
@@ -32,28 +42,40 @@ for i in range(rows):
             counter_without_problems = 0
             if image[i][j+1][0] == 0 and image[i][j+1][1] == 0 and image[i][j+1][2] == 0:
                 new_color = list(np.random.choice(range(256), size=3)) #losuje nowy kolor jak znajdzie nowy bialy kolor
-                counter_of_colors+=1
+                list_of_colors.append(generateColor(256,3))
+                #counter_of_colors+=1
+            
             if(i>0):
                 if (image[i-1][j][0] != 0 and image[i-1][j][1] != 0 and image[i-1][j][2] != 0):
                     image[i][j] = image[i-1][j]
+                    
                 elif(image[i][j-1][0] != 0 and image[i][j-1][1] != 0 and image[i][j-1][2] != 0):
                     image[i][j] = image[i][j-1] 
+                    list_of_colors = list_of_colors[:-1] 
+                    
                 elif(image[i-1][j-1][0] != 0 and image[i-1][j-1][1] != 0 and image[i-1][j-1][2] != 0):
                     image[i][j] = image[i-1][j-1] 
+                    list_of_colors = list_of_colors[:-1] 
+                 
                 elif(image[i-1][j+1][0] != 0 and image[i-1][j+1][1] != 0 and image[i-1][j+1][2] != 0):
                     image[i][j] = image[i-1][j+1]
+                    list_of_colors = list_of_colors[:-1] 
+                 
                 else:
                     counter_without_problems = 0
+                    list_of_colors = list_of_colors[:-1] 
                 
                # if image[i][j+1][0] == 255 and image[i][j+1][1] == 255 and image[i][j+1][2] == 255:
                #     counter_without_problems+=1
-                              
+                     
     for k in range(cols1):
         if(k<cols1):
             if(image[i][cols1-k-1][0] == 0 and image[i][cols1-k-1][1] == 0 and image[i][cols1-k-1][2] == 0):
                 continue
         if((image[i][cols1-k][0] != 0 and image[i][cols1-k][1] != 0 and image[i][cols1-k][2] != 0) and (image[i][cols1-k][0] != 255 and image[i][cols1-k][1] != 255 and image[i][cols1-k][2] != 255)):  #patrz na prawo jeśli kolor nie jest czarny lub bialy to wtedy trzeba sie cofnac kolorkami
             image[i][(cols1-k)-1] = image[i][cols1-k]
+    
+    #list_of_colors = list_of_colors[:-1]        
         
        ##działa kod
 
@@ -63,10 +85,21 @@ for i in range(rows):
      
             #while((image[i][j][0] == 255 and image[i][j][1] == 255 and image[i][j][2] == 255) and (image[i+1][j][0] == 255 and image[i+1][j][1] == 255 and image[i+1][j][2] == 255) and i < 512 and j < 512):
                # image_new[i][j] = 254,128,128
-                
+               
+               #eksperymentalnie \/
+for l in range(rows):
+    i_i = rows-l -1            
+    for k in range(cols1):
+        if(k<cols1):
+            if(image[i_i][cols1-k-1][0] == 0 and image[i_i][cols1-k-1][1] == 0 and image[i_i][cols1-k-1][2] == 0):
+                continue
+        if((image[i_i][cols1-k][0] != 0 and image[i_i][cols1-k][1] != 0 and image[i_i][cols1-k][2] != 0) and (image[i_i][cols1-k][0] != 255 and image[i_i][cols1-k][1] != 255 and image[i_i][cols1-k][2] != 255)):  #patrz na prawo jeśli kolor nie jest czarny lub bialy to wtedy trzeba sie cofnac kolorkami
+            image[i_i][(cols1-k)-1] = image[i_i][cols1-k]
+        elif((image[i_i+1][cols1-k][0] != 0 and image[i_i+1][cols1-k][1] != 0 and image[i_i+1][cols1-k][2] != 0) and (image[i_i+1][cols1-k][0] != 255 and image[i_i+1][cols1-k][1] != 255 and image[i_i+1][cols1-k][2] != 255)):  #patrz na prawo jeśli kolor nie jest czarny lub bialy to wtedy trzeba sie cofnac kolorkami
+            image[i_i][(cols1-k)-1] = image[i_i-1][cols1-k]                  
 
 
 cv2.imshow('Image', image_new)
-ctypes.windll.user32.MessageBoxW(0, "Detected objects: " + str(counter_of_colors) + "\n\nAutorzy:\tAlbert Pintera, Jakub Kaniowski\n| 2021 | Polsl | AiR TI-3 |", "Information " , 1)
+ctypes.windll.user32.MessageBoxW(0, "Detected objects: " + str(counter_of_colors) + "\n\nAutorzy:\tAlbert Pintera, Jakub Kaniowski\n| 2021 | Polsl | AiR TI-3 |" + str(len(list_of_colors)), "Information " , 1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
