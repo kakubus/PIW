7a6e15100d7415279c8a06a6c41e2f191d03c116
Kuba <kakubus@outlook.com>
2021-11-18 18:13:26 +0100

    Poprawki

--- main.py
+++ main.py
@@ -1,71 +1,54 @@
 import cv2
 import random
 import numpy as np
-import ctypes  # An included library with Python install.   
+import ctypes  
 import time
 
-start_time = time.time()
+start_time = time.time()    #Rozpoczecie zliczania czasu wykonania programu
 
-counter_of_colors = 0
-list_of_colors = []
+counter_of_objects = 0      #Licznik obiektow
+list_of_colors = []         #Lista kolorow
 
-def generateColor(rng, i):
-    global counter_of_colors
-    #counter_of_colors+=1
-    return (np.random.choice(range(rng), size=i))
+#Lista kolorow - pierwszy nowy kolor
+new_color = list(np.random.choice(range(256), size=3)) 
 
-image = cv2.imread('shapes3.png', 0)
+image = cv2.imread('shapes4_hard.png', 0)                #Wczytanie obrazu (sciezka)
+image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)     #Zamiana barw na obrazie
 
-image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-
-
-#list_of_colors.append((64,64,64)) #poczytac o listach, bo trzeba zrobic zliczanie kolorkow
-
-counter_without_problems = 0
-
-new_color = list(np.random.choice(range(256), size=3))
-#list_of_colors.append(generateColor(256,3))
-
+#Wymiary obrazu
 rows = image.shape[0]
 cols = image.shape[1]
-n_i = 0
-n_j = 0
+
 cols1 = image.shape[1]-1
+
+#Funkcja losujaca kolejne kolory
+def generateColor(rng, i):
+    global counter_of_objects
+    return (np.random.choice(range(rng), size=i))
+
 for i in range(rows):
     for j in range(cols):
         
-        if image[i][j][0] == 255 and image[i][j][1] == 255 and image[i][j][2] == 255:
+        if image[i][j][0] == 255 and image[i][j][1] == 255 and image[i][j][2] == 255:                   # 255 - czarny kolor
             image[i][j] = new_color
             
-            counter_without_problems = 0
-            if image[i][j+1][0] == 0 and image[i][j+1][1] == 0 and image[i][j+1][2] == 0:
-                new_color = generateColor(256,3) #losuje nowy kolor jak znajdzie nowy bialy kolor
-               # list_of_colors.append(new_color)
-               # counter_of_colors+=1
-            
+            if image[i][j+1][0] == 0 and image[i][j+1][1] == 0 and image[i][j+1][2] == 0:               # 0 - biały kolor
+                new_color = generateColor(256,3)                                                        #Losuje nowy kolor jak znajdzie nowy bialy kolor
+                
             if(i>0):
-                if (image[i-1][j][0] != 0 and image[i-1][j][1] != 0 and image[i-1][j][2] != 0):
+                if (image[i-1][j][0] != 0 and image[i-1][j][1] != 0 and image[i-1][j][2] != 0):         #Algorytm rozglada sie wyzej, na poprzedni wiersz od aktualnego miejsca
                     image[i][j] = image[i-1][j]
                     
-                elif(image[i][j-1][0] != 0 and image[i][j-1][1] != 0 and image[i][j-1][2] != 0):
+                elif(image[i][j-1][0] != 0 and image[i][j-1][1] != 0 and image[i][j-1][2] != 0):        #Algorytm rozglada sie w lewo od aktualnego miejsca
                     image[i][j] = image[i][j-1]
-                    #list_of_colors = list_of_colors[:-1]
                     
-                elif(image[i-1][j-1][0] != 0 and image[i-1][j-1][1] != 0 and image[i-1][j-1][2] != 0):
+                elif(image[i-1][j-1][0] != 0 and image[i-1][j-1][1] != 0 and image[i-1][j-1][2] != 0):  #Algorytm rozglada sie w lewo w poprzednim wierszu od aktualnego miejsca (lewy gorny rog)
                     image[i][j] = image[i-1][j-1]
-                    #list_of_colors = list_of_colors[:-1] 
                  
-                elif(image[i-1][j+1][0] != 0 and image[i-1][j+1][1] != 0 and image[i-1][j+1][2] != 0):
-                    image[i][j] = image[i-1][j+1]
-                    #list_of_colors = list_of_colors[:-1] 
-                 
-                else:
-                    counter_without_problems = 0
-                    #list_of_colors = list_of_colors[:-1] 
-                
-               # if image[i][j+1][0] == 255 and image[i][j+1][1] == 255 and image[i][j+1][2] == 255:
-               #     counter_without_problems+=1
+                elif(image[i-1][j+1][0] != 0 and image[i-1][j+1][1] != 0 and image[i-1][j+1][2] != 0):  #Algorytm rozglada sie w prawo w poprzednim wierszu od aktualnego miejsca (prawy gorny rog)
+                    image[i][j] = image[i-1][j+1]        
                      
+    #Ponowny odczyt wiersza, z przeciwnej strony (od prawej do lewej - ujednolica kolory)           
     for k in range(cols1):
         if(k<cols1):
             if(image[i][cols1-k-1][0] == 0 and image[i][cols1-k-1][1] == 0 and image[i][cols1-k-1][2] == 0):
@@ -73,32 +56,9 @@ for i in range(rows):
             if((image[i][cols1-k][0] != 0 and image[i][cols1-k][1] != 0 and image[i][cols1-k][2] != 0) and (image[i][cols1-k][0] != 255 and image[i][cols1-k][1] != 255 and image[i][cols1-k][2] != 255)):  #patrz na prawo jeśli kolor nie jest czarny lub bialy to wtedy trzeba sie cofnac kolorkami
                 image[i][(cols1-k)-1] = image[i][cols1-k]
 
-    
-    #list_of_colors = list_of_colors[:-1]        
-        
-       ##działa kod
 
-                  
-    
-                                                
-     
-            #while((image[i][j][0] == 255 and image[i][j][1] == 255 and image[i][j][2] == 255) and (image[i+1][j][0] == 255 and image[i+1][j][1] == 255 and image[i+1][j][2] == 255) and i < 512 and j < 512):
-               # image_new[i][j] = 254,128,128
-               
-               #eksperymentalnie \/
-#for l in range(rows):
-    #i_i = rows-l -1            
-   # for k in range(cols1):
-   #     if(k<cols1):
-    #        if(image[i_i][cols1-k-1][0] == 0 and image[i_i][cols1-k-1][1] == 0 and image[i_i][cols1-k-1][2] == 0):
-    #            continue
-   #     if((image[i_i][cols1-k][0] != 0 and image[i_i][cols1-k][1] != 0 and image[i_i][cols1-k][2] != 0) and (image[i_i][cols1-k][0] != 255 and image[i_i][cols1-k][1] != 255 and image[i_i][cols1-k][2] != 255)):  #patrz na prawo jeśli kolor nie jest czarny lub bialy to wtedy trzeba sie cofnac kolorkami
-   #         image[i_i][(cols1-k)-1] = image[i_i][cols1-k]
-   #     elif((image[i_i+1][cols1-k][0] != 0 and image[i_i+1][cols1-k][1] != 0 and image[i_i+1][cols1-k][2] != 0) and (image[i_i+1][cols1-k][0] != 255 and image[i_i+1][cols1-k][1] != 255 and image[i_i+1][cols1-k][2] != 255)):  #patrz na prawo jeśli kolor nie jest czarny lub bialy to wtedy trzeba sie cofnac kolorkami
-   #         image[i_i][(cols1-k)-1] = image[i_i-1][cols1-k]          
-   # 
-
-for i in range(rows):
+#Ponowny odczyt calego obrazu w celu zliczenia wystepujacych obiektow
+for i in range(rows): 
     for j in range(cols):
         if (image[i][j][0] != 255 and image[i][j][1] != 255 and image[i][j][2] != 255) and (image[i][j][0] != 0 and image[i][j][1] != 0 and image[i][j][2] != 0):
             if any(np.array_equal([image[i][j][0],image[i][j][1],image[i][j][2]], x) for x in list_of_colors):
@@ -106,11 +66,16 @@ for i in range(rows):
             else:
                 list_of_colors.append(np.array([image[i][j][0],image[i][j][1],image[i][j][2]]))        
 
-counter_of_colors = len(list_of_colors)
+counter_of_objects = len(list_of_colors)    #Przypisanie liczby zliczonych kolorow do zmiennej przechowujacej liczbe obiektow
 
-print(list_of_colors)
+#Wyswietlenie uzyskanych wynikow
+cv2.imshow('Processed Image', image)
 
-cv2.imshow('Image', image)
-ctypes.windll.user32.MessageBoxW(0, "Detected objects: " + str(counter_of_colors) + "\n\nAutorzy:\tAlbert Pintera, Jakub Kaniowski\n| 2021 | Polsl | AiR TI-3 |" + str(len(list_of_colors)) + "\n\nTime of execution: " + str(float("{0:.2f}".format(time.time() - start_time))) + " seconds", "Information " , 1)
+#Dla kazdego systemu
+print("Program info: \n\nDetected objects: " + str(counter_of_objects) + "\nAuthors:\tAlbert Pintera, Jakub Kaniowski\n| 2021 | Polsl | AiR TI-3 |" + "\n\nTime of execution: " + str(float("{0:.2f}".format(time.time() - start_time))) + " seconds")
+
+#Dla windowsa (okomentowac w razie potrzeby)
+ctypes.windll.user32.MessageBoxW(0, "Detected objects: " + str(counter_of_objects) + "\nAuthors:\tAlbert Pintera, Jakub Kaniowski\n| 2021 | Polsl | AiR TI-3 |" + "\n\nTime of execution: " + str(float("{0:.2f}".format(time.time() - start_time))) + " seconds", "Information " , 1)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
